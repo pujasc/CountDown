4ecd35221042f12b516dc194f89b5514c68052f3
pujasc <chandelsinghpuja@gmail.com>
2020-10-05 13:18:14 +0530

    added unsplash request

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import datetime
 from kivy.uix.image import AsyncImage 
 from kivy.uix.floatlayout import FloatLayout # import FloatLayout  
 
+from kivy.config import Config  
+Config.set('graphics', 'resizable', True)
+
 def daycount():
         today = datetime.datetime.now()
         NewYear = datetime.datetime(year=2021,month=1,day=1)
@@ -17,9 +20,15 @@ class MainApp(App):
     def build(self):
         Layout = FloatLayout(size = (300,300)) 
         label = Label(text= daycount()+' days left in New Year 2021!!!',
-                      size_hint=(1, 1),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+                      pos=(50,-250),
+                      font_size='30sp')
         Layout.add_widget(label)
+        
+        Image = AsyncImage(source ='https://source.unsplash.com/random/?motivational',
+                            pos=(0,100)) 
+        Image.allow_stretch = True
+        Image.keep_ratio = False
+        Layout.add_widget(Image)
         return Layout
 
 if __name__ == '__main__':
